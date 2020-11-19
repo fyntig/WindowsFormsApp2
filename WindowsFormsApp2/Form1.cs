9372e65484a55af3f7fa3da0498280f4fdd8257b
Антон Огнев <fynjy-tox@ya.ru>
2020-11-19 16:32:44 +0300

    циклы по картинке

--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -12,7 +12,7 @@ namespace WindowsFormsApp2
 {
     public partial class Form1 : Form
     {
-        Bitmap b1, b2 = new Bitmap(200, 200);
+        Bitmap b1 = new Bitmap(200, 200), b2 = new Bitmap(200, 200);
 
         Graphics g1, g2;
         Point CurrentPoint, PrevPoint;
@@ -32,12 +32,14 @@ namespace WindowsFormsApp2
                 CurrentPoint = e.Location;
                 Pen _p = new Pen(Color.Black);
                 g1.DrawLine(_p, PrevPoint, CurrentPoint);
+                panel1.BackgroundImage = b1;
+                panel1.Refresh();
             }
         }
 
         private void panel1_MouseUp(object sender, MouseEventArgs e)
         {
-            isPressed = false;
+            isPressed = false;           
         }
 
         private void panel2_MouseDown(object sender, MouseEventArgs e)
@@ -54,6 +56,8 @@ namespace WindowsFormsApp2
                 CurrentPoint = e.Location;
                 Pen _p = new Pen(Color.Black);
                 g2.DrawLine(_p, PrevPoint, CurrentPoint);
+                panel2.BackgroundImage = b2;
+                panel2.Refresh();
             }
         }
 
@@ -66,13 +70,13 @@ namespace WindowsFormsApp2
         {
             InitializeComponent();
             g1 = Graphics.FromImage(b1);
-            g2 = Graphics.FromImage(b2);
+            g2 = Graphics.FromImage(b2);           
         }
 
         class xPoint
         {
             public int x, y, //координата
-                        t,  //тип: 1 - пересечение, 2 - окончание, 3 - точка линии
+                        t,  //тип: 1 - пересечение, 2 - окончание, 3 - точка линии, 0 - точка
                         v;  //направление - как часы: 1, 3 ,5 ,6 ,7 ,9 ,11, 12
             public double r; //степень близости к наиболее близкой точке второго рисунка
 
@@ -81,24 +85,96 @@ namespace WindowsFormsApp2
           
         }
 
-        List<xPoint> i1 = new List<xPoint>();
-        List<xPoint> i2 = new List<xPoint>();
-
         private void button1_Click(object sender, EventArgs e)
         {
+            List<xPoint> i1 = new List<xPoint>();
+            List<xPoint> i2 = new List<xPoint>();
 
-            //это код который определяет чёрный пиксель
-            for(int _x = 0; _x < 200; _x++)
-            for(int _y = 0; _y < 200; _y++)
-            if(b1.GetPixel(_x, _y) == Color.Black)
-                {
-                //определяем тип этого пикселя пересечение, окончание или линия
+            Pen _p = new Pen(Color.Black);
+           
 
 
-                }
+            //это код который определяет чёрный пиксель (крайние игнорируются, потому что он всегда будет окончанием или пересечением)
+            for (int _x = 1; _x < 199; _x++)
+            for (int _y = 1; _y < 199; _y++)
 
 
-           // foreach (xPoint i in i1)
+            if (b1.GetPixel(_x, _y) == Color.Black)      
+            
+
+                        {
+                            int _v = 0, _s = -1, __v = 1; //направление окончания и сумма соседей 0 - точка, 1 - окончание, 2 - линия 3+ - пересечение
+                                                          //определяем тип этого пикселя пересечение, окончание или линия
+                                                          //определим его соседей
+                            for (int __x = -1; __x < 2; __x++)
+                                for (int __y = -1; __y < 2; __y++)
+                                {
+                                    __v++;
+                                    if (b1.GetPixel(_x + __x, _y + __y) == Color.Black)
+                                    {
+                                        _s++;
+                                        if (__x != 0 && __y != 0) _v = __v;
+                                    }
+                                }
+
+                                 switch (_s)
+                                 {
+                                     case 0:
+                                         i1.Add(new xPoint(_x, _y, 0));
+                                         break;
+                                     case 1:
+                                         i1.Add(new xPoint(_x, _y, 2, _v));
+                                         break;
+                                     case 2:
+                                         i1.Add(new xPoint(_x, _y, 3));
+                                         break;
+                                     default:
+                                         MessageBox.Show("!!");
+                                         i1.Add(new xPoint(_x, _y, 1));
+                                         break;
+                                 } 
+
+
+                        }
+                    
+
+
+            for (int _x = 1; _x < 199; _x++)
+                for (int _y = 1; _y < 199; _y++)
+                    if (b2.GetPixel(_x, _y) == Color.Black)
+                    {
+                        
+                        int _v = 0, _s = -1, __v = 1; //направление окончания и сумма соседей 0 - точка, 1 - окончание, 2 - линия 3+ - пересечение
+                                                      //определяем тип этого пикселя пересечение, окончание или линия
+                                                      //определим его соседей
+                        for (int __x = -1; __x < 2; __x++)
+                            for (int __y = -1; __y < 2; __y++)
+                            {
+                                __v++;
+                                if (b2.GetPixel(_x+__x, _y+__y) == Color.Black)
+                                {
+                                    _s++;
+                                    if (__x != 0 && __y != 0) _v = __v;
+                                }
+                            }
+
+                        if (_s == 0)
+                                i2.Add(new xPoint(_x, _y, 0));
+                        if (_s == 1) {
+                            MessageBox.Show("!");
+                            i2.Add(new xPoint(_x, _y, 2, _v));
+                        }
+                        if (_s == 2)
+                            i2.Add(new xPoint(_x, _y, 3));
+                        if (_s > 2)
+                            i2.Add(new xPoint(_x, _y, 1));
+                             
+                        
+
+                    }
+
+
+            // foreach (xPoint i in i1)
             //    MessageBox.Show(i.x.ToString()); ;
         }
     }
